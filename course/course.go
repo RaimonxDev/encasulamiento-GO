0629145730c6388d0641bf74f7efeca77ff0d57e
Ramon Martinez Work <ramonmartinezcordero@gmail.com>
2022-06-23 11:17:54 -0400

    Modulo de course agregado

---
+++ course/course.go
@@ -0,0 +1,26 @@
+package course
+
+import "fmt"
+
+type Course struct {
+	Name    string
+	Price   float64
+	IsFree  bool
+	UserID  []uint
+	Classes map[uint]string
+}
+
+//Podemos llamar los desde otros archivos
+// Deben de pertenecer a el mismo package
+func (c Course) PrintClasses() {
+	text := "Las clases son las siguientes: "
+	for _, class := range c.Classes {
+		text += class + ", "
+	}
+	fmt.Println(text[:len(text)-2])
+}
+
+/* Para poder actualizar un campo debemos de recibir un puntero de la interface a trabajar */
+func (c *Course) ChangePrice(price float64) {
+	c.Price = price
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module encasulamiento
+
+go 1.18
